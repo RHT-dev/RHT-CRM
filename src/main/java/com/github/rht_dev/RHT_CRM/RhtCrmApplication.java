1cd3b6e572660f47978149a03ec35301eb963781
royal <royalhellt@gmail.com>
2025-09-21 17:00:40 +0500

    chore: initial project setup from Spring Initializr

---
+++ src/main/java/com/github/rht_dev/RHT_CRM/RhtCrmApplication.java
@@ -0,0 +1,13 @@
+package com.github.rht_dev.RHT_CRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhtCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhtCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rht_dev/RHT_CRM/RhtCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rht_dev.RHT_CRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhtCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
